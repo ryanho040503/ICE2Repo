b2bbc8f10abc6ebce4a68c9ae95740b4cddbf591
macbookairm1 <macbookairm1@localhost>
2025-10-02 09:39:33 -0400

    Rev 1 local

---
+++ src/main/java/com/mycompany/ice2_testgit/ICE2_TestGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice2_testgit;
+
+/**
+ *
+ * @author macbookairm1
+ */
+public class ICE2_TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Rev 1 local!");
+    }
+}
